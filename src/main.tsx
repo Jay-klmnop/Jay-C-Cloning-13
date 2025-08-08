8dca389e6af270c89c73476cf80ef8ca84c79b4d
Jay <jiyeyoun@outlook.com>
2025-08-08 23:38:54 +0900

    fix(deploy): Configure base path for GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Jay-C-Cloning-13/">
       <App />
     </BrowserRouter>
   </StrictMode>,
